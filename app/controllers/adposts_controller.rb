8272b73b0dc2ec8e54bc90be9c3ef76e8c6d0bfe
Ndelforno <nicolas.delforno@gmail.com>
2019-01-01 18:55:40 -0500

    actibe=ve storage configure + create method added for adpost

--- app/controllers/adposts_controller.rb
+++ app/controllers/adposts_controller.rb
@@ -9,6 +9,15 @@ class AdpostsController < ApplicationController
     @adpost.title = params[:adpost][:title]
     @adpost.description = params[:adpost][:description]
     @adpost.price = params[:adpost][:price]
-
+    @adpost.address = params[:adpost][:address]
+    @adpost.user = current_user
+    @adpost.category_id = params[:category_id]
+    @adpost.image.attach(params[:adpost][:image])
+    if adpost.save
+      redirect_to adpost_path
+    else
+      render :new
+    end
   end
+
 end
