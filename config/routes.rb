1cab802a2429fb038554757c5687c2074a4f0254
Ndelforno <nicolas.delforno@gmail.com>
2019-01-01 17:21:12 -0500

    Form for user creation added

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root 'pages#index'
   get 'pages/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  ressources :users
-  ressources :adposts
-  ressources :categories
+  resources :users
+  resources :adposts
+  resources :categories
 end
