9826647afb7d52debd3c106dd400b6edb585b615
Ndelforno <nicolas.delforno@gmail.com>
2019-01-03 16:15:15 -0500

    dReact router added

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,8 +1,32 @@
 import React from 'react'
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom';
+
+
 class App extends React.Component {
   render () {
     return (
-      <div>Hello World</div>
+      <Router>
+        <div>
+          <h1>Hello World</h1>
+          <nav>
+          <Link to="">Appliances</Link>{' '}
+          <Link to="">Cars-Trucks</Link>{' '}
+          <Link to="">Books</Link>{' '}
+          <Link to="">Video-games</Link>{' '}
+          <Link to="">Furnitures</Link>{' '}
+          <Link to="/categories/8">Computers</Link>{' '}
+          <Link to="">Electronics</Link>{' '}
+          <Link to="">Motorcycle</Link>{' '}
+          <Link to="">Cell Phone</Link>{' '}
+          <Link to="">Bikes</Link>{' '}
+          <Link to="">Tickets</Link>
+          </nav>
+        </div>
+      </Router>
     )
   }
 }
