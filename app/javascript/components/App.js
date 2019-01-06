64520a4fc3f66a8809f1acb03f1533b24afe5c1b
Ndelforno <nicolas.delforno@gmail.com>
2019-01-06 16:54:44 -0500

    Categories added to apps.js

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -9,19 +9,39 @@ import Categories from '../components/Categories'
 
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      categories: []
+    };
+  }
+  componentDidMount(){
+    fetch('/api/v1/categories.json')
+      .then((response) => {return response.json()})
+      .then((data) => {this.setState({ categories: data }) });
+  }
+
   render () {
+
+    var categories = this.state.categories.map((category) => {
+      return(
+        <Router>
+           <div class= "card category" id = {category.name} key={category.id} >
+            <nav>
+              <h1><Link to= {category.name}> {category.name}</Link></h1>
+            </nav>
+              <Route path= {category.name} component= {category.name} />
+           </div>
+         </Router>
+        )
+    })
     return (
-      <Router>
-        <div>
+      <div>
+        <h1>To do: List of Categories</h1>
           <div class="categories-container">
-            <div class= "card category" id = "Computers" >
-              <h1><Link to= "/Computers">Computers</Link></h1>
-            </div>
-            <Route path= "/Computers" component= {Computers} />
+              {categories}
           </div>
-          
-        </div>
-      </Router>
+      </div>
     )
   }
 }
