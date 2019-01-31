9d0bd18ce1cefa21656ba1df7545a5b400f4ea20
Ndelforno <nicolas.delforno@gmail.com>
2019-01-31 16:57:50 -0500

    removed devise authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,13 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  # devise :database_authenticatable, :registerable,
+  #        :recoverable, :rememberable, :validatable
   has_secure_password
 
   # model association
   has_many :adposts, dependent: :destroy
 
   # validations
-  validates_presence_of :user_name, :email, :password 
+  validates_presence_of :user_name, :email, :password
 end
